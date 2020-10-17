f6c18dd83d70d852e19c8ccd50fe013ec4198e20
Leandro Jr <leandrojr70@gmail.com>
2020-10-17 15:15:41 -0300

    refatoracao de codigo em paginas separadas

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,36 @@
+import 'package:despesas_pessoais/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'transaction_form.dart';
+import 'transaction_list.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tenis de Corrida',
+      value: 310.16,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Novo Tenis de Corrida',
+      value: 310.16,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
+    );
+  }
+}
